d9a06e51b7c0ecc6f0090cc91017d9404b5f8ceb
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2024-09-11 10:18:38 -0300

    fix: Fixed AVL tree code

--- Exercises/AVL Tree/main.c
+++ Exercises/AVL Tree/main.c
@@ -8,7 +8,7 @@ struct Node {
     struct Node *right;
 };
 
-int getHeight(struct Node *node) {
+int GetHeight(struct Node *node) {
     if (node == NULL)
         return -1;
     return node->height;
@@ -17,7 +17,7 @@ int getHeight(struct Node *node) {
 int BalanceFactor(struct Node *node) {
     if (node == NULL)
         return 0;
-    return getHeight(node->left) - getHeight(node->right);
+    return GetHeight(node->left) - GetHeight(node->right);
 }
 
 int Max(int n1, int n2) {
@@ -29,8 +29,8 @@ struct Node *LeftRotation(struct Node *node) {
     node->left = aux->right;
     aux->right = node;
 
-    node->height = Max(getHeight(node->left), getHeight(node->right)) + 1;
-    aux->height = Max(getHeight(aux->left), getHeight(aux->right)) + 1;
+    node->height = Max(GetHeight(node->left), GetHeight(node->right)) + 1;
+    aux->height = Max(GetHeight(aux->left), GetHeight(aux->right)) + 1;
 
     return aux;
 }
@@ -40,8 +40,8 @@ struct Node *RightRotation(struct Node *node) {
     node->right = aux->left;
     aux->left = node;
 
-    node->height = Max(getHeight(node->left), getHeight(node->right)) + 1;
-    aux->height = Max(getHeight(aux->left), getHeight(aux->right)) + 1;
+    node->height = Max(GetHeight(node->left), GetHeight(node->right)) + 1;
+    aux->height = Max(GetHeight(aux->left), GetHeight(aux->right)) + 1;
 
     return aux;
 }
@@ -59,60 +59,132 @@ struct Node *CreateNode(int data) {
     return node;
 }
 
-// Função para imprimir a árvore
-void printTree(struct Node *node, int lvl) {
+void PrintTree(struct Node *node, int lvl) {
     if (node != NULL) {
         for (int i = 0; i < lvl; i++)
             printf("  ");
 
         printf("%d\n", node->data);
-        printTree(node->left, lvl + 1);
-        printTree(node->right, lvl + 1);
+        PrintTree(node->left, lvl + 1);
+        PrintTree(node->right, lvl + 1);
     }
 }
 
 struct Node *InsertAVL(struct Node *root, int data) {
-    // Step 1 - Inserção de árvore BB
-    if (root == NULL)
-        return CreateNode(data);
+  // Step 1 - Inserção de árvore BB
+  if (root == NULL)
+      return CreateNode(data);
 
-    if (data < root->data)
-        root->left = InsertAVL(root->left, data);
-    else if (data > root->data)
-        root->right = InsertAVL(root->right, data);
+  if (data < root->data)
+      root->left = InsertAVL(root->left, data);
+  else if (data > root->data)
+      root->right = InsertAVL(root->right, data);
 
-    // Step 2 - Balanceamento a cada inserção
-    root->height = Max(getHeight(root->left), getHeight(root->right)) + 1;
-    int bf = BalanceFactor(root);
+  // Step 2 - Balanceamento a cada inserção
+  root->height = Max(GetHeight(root->left), GetHeight(root->right)) + 1;
+  int bf = BalanceFactor(root);
 
-    if (bf > 1) {
-        if (data < root->left->data)
-            return LeftRotation(root);
-        else {
-            root->left = RightRotation(root->left);
-            return LeftRotation(root);
-        }
-    } else if (bf < -1) {
-        if (data > root->right->data)
-            return RightRotation(root);
-        else {
-            root->right = LeftRotation(root->right);
-            return RightRotation(root);
-        }
+  if (bf > 1) {
+      if (data < root->left->data)
+          return LeftRotation(root);
+      else {
+          root->left = RightRotation(root->left);
+          return LeftRotation(root);
+      }
+  } else if (bf < -1) {
+      if (data > root->right->data)
+          return RightRotation(root);
+      else {
+          root->right = LeftRotation(root->right);
+          return RightRotation(root);
+      }
+  }
+
+  return root;
+}
+
+struct Node *FindBiggestRight(struct Node *node) {
+  struct Node *aux = node;
+
+  while (aux->right != NULL) {
+    aux = aux->right;
+  }
+
+  return aux;
+}
+
+struct Node *RemoveNode(struct Node *node) {
+  struct Node *aux;
+  
+  if (node->left == NULL)
+    aux = node->right;
+  else if (node->right == NULL)
+    aux = node->left;
+  else {
+    aux = FindBiggestRight(node->left);
+    struct Node *aux2 = node->left;
+
+    if (aux != aux2) {
+      aux2->right = aux->left;
+      aux->left = aux2;
     }
+    aux->right = node->right;
+  }
 
-    return root;
+  free(node);
+  return aux;
+}
+
+struct Node *RemoveAVL(struct Node *root, int data) {
+  // Step 1 - Remoção de árvore BB
+  if (root == NULL) return root;
+  
+  if (root->data > data)
+    root->left = RemoveAVL(root->left, data);
+  else if (root->data < data)
+    root->right = RemoveAVL(root->right, data);
+  else
+    return RemoveNode(root);
+    
+  // Step 2 - Balanceamento a cada inserção
+  root->height = Max(GetHeight(root->left), GetHeight(root->right)) + 1;
+  int bf = BalanceFactor(root);
+  
+  if (bf > 1) {
+      if (data < root->left->data)
+          return LeftRotation(root);
+      else {
+          root->left = RightRotation(root->left);
+          return LeftRotation(root);
+      }
+  } else if (bf < -1) {
+      if (data > root->right->data)
+          return RightRotation(root);
+      else {
+          root->right = LeftRotation(root->right);
+          return RightRotation(root);
+      }
+  }
+  
+  return root;
 }
 
 int main() {
-    struct Node *root = NULL;
+  struct Node *root = NULL;
 
-    int v[7] = {30, 9, 45, 27, 3, 18, 35};
-    
-    for (int i = 0; i < 7; i++) {
-        root = InsertAVL(root, v[i]);
-    }
+  int v[7] = {30, 9, 45, 27, 3, 18, 35};
+  
+  for (int i = 0; i < 7; i++) {
+      root = InsertAVL(root, v[i]);
+  }
 
-    printTree(root, 0);
-    return 0;
+  PrintTree(root, 0);
+  
+  root = RemoveAVL(root, 3);
+  root = RemoveAVL(root, 9);
+  root = RemoveAVL(root, 18);
+  
+  PrintTree(root, 0);
+  
+  return 0;
 }
